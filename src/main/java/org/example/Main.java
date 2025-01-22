9927952adec4d59eb790cc3f877cb0e202fbc073
Mustafa <mstf.fdn@outlook.com>
2025-01-22 22:19:07 +0300

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+//        Grocery grocery = new Grocery();
+//        grocery.startGrocery();
+
+        MobilePhone mobilePhone = new MobilePhone("123123124", new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("mustafa", "132091203123"));
+        mobilePhone.printContacts();
 
     }
 }
